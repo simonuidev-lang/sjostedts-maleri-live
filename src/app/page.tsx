240d7cf2bb1cd4614680002dce3ea38213416a9b
Simon <krigaren109@gmail.com>
2026-05-30 21:42:27 +0200

    Jonas in the right folder

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-9xl font-bold">JONAS</h1>
+    </div>
+  );
+}
